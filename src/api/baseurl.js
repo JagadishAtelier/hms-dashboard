08e1aebf27b0395d078bbca648d37df1d76852de
Gnanaparthiban M <gnanaparthiban9655@gmail.com>
2026-04-10 18:31:59 +0530

    pos

--- src/api/baseurl.js
+++ src/api/baseurl.js
@@ -5,8 +5,6 @@
 
 // export const BASE_API = "https://hms-be-0a13.onrender.com/api/v1";
 
-//export const BASE_API = "http://10.143.164.85:4000/api/v1";
-//export const BASE_API = "http://172.31.224.1:4000/api/v1";
 
 
 export default BASE_API;
